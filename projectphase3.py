1e793d14c7138a27dc2a6c000edf04b297f71331
ashly-usc <ashlylam@usc.edu>
2024-04-30 20:53:41 -0700

    Update projectphase3.py

--- projectphase3.py
+++ projectphase3.py
@@ -26,20 +26,20 @@ data = data[["G1", "G2", "G3", "age", "Medu", "Fedu"]]
 
 predict = "G3"
 
-X = np.array(data.drop([predict], axis=1))  # Specify axis=1 to drop along columns
-y = np.array(data[predict])  # Labels
+X = np.array(data.drop([predict], axis=1))
+y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
 
 linear = linear_model.LinearRegression()
 linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test) # acc stands for accuracy
+acc = linear.score(x_test, y_test)
 print(acc)
 
-print('Coefficient: \n', linear.coef_) # These are each slope value
-print('Intercept: \n', linear.intercept_) # This is the intercept
+print('Coefficient: \n', linear.coef_)
+print('Intercept: \n', linear.intercept_)
 
-predictions = linear.predict(x_test) # Gets a list of all predictions
+predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
